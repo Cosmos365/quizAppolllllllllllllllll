b736aa8ec9a8bdd0a8f2234d8cdd44cb595a6e2f
Avi <81462673+Cosmos365@users.noreply.github.com>
2022-09-13 12:48:35 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function start() {
+    window.location = "Question1.html";
+}
+
+function q2() {
+    window.location = "Question2.html";
+}
+
+function q3() {
+    window.location = "Question3.html";
+}
+
+function q4() {
+    window.location = "Question4.html";
+}
+
+function q5() {
+    window.location = "Question5.html";
+}
+
+function q6() {
+    window.location = "Question6.html";
+}
+
+function q7() {
+    window.location = "Question7.html";
+}
+
+function fail() {
+    window.location = "YouLoseBozo.html";
+}
+
+function w() {
+    window.location = "W.html";
+}
+
+function GoBack() {
+    window.location = "Index.html"; 
+}
